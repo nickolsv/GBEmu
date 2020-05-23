d94132c43d6044e202d856fcdb0a2e4e95fe84e8
nickolsv <nickols1234@gmail.com>
2020-05-23 17:37:33 +0300

    Implement DEC A

--- src/ops.cpp
+++ src/ops.cpp
@@ -1168,7 +1168,17 @@ int cpu::op_DEC_A(void)
     //      - Sets H if borrows from bit 4
     // 4 Cycles, 1 byte
 
-    // TODO: Implement
+    registers["AF"]->decrementHighRegister();
+
+    uint8_t val = registers["AF"]->getHighValue();
+
+    if( val == 0 )              setFlag('Z');
+    else if( val % 16 == 15 )   setFlag('H');           // When decrementing a number, the only case
+                                                        // bit 4 is borrowed from is when a number of the form
+                                                        // XXX10000 is decremented to XXX01111, therefore
+                                                        // the result mod 16 equals 15
+
+    setFlag('N');
 
     return 4;
 }
